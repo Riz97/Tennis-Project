94ca767861e67db847fb1b4e82375172f6785c6b
Riccardo Caprile <93477707+Riz97@users.noreply.github.com>
2024-03-22 01:14:15 +0100

    Update Tennis.py

--- Tennis.py
+++ Tennis.py
@@ -1,19 +1,26 @@
 import tkinter as tk
 from tkinter import filedialog
 import csv
+import pandas as pd
+from tkinter import ttk
+
+flag = 0
 
 # Function to open a specific .csv file
 def open_csv_file():
    file_path = filedialog.askopenfilename(defaultextension=".csv", filetypes=[("CSV Files", "*.csv")])
-   if file_path:
-      try:
-         with open(file_path, "r") as csv_file:
-            # Read and display the CSV file's contents
-            csv_reader = csv.reader(csv_file)
-            for row in csv_reader:
-               print(row)
-      except Exception as e:
-         print(f"Error: {e}")
+   df = pd.read_csv(file_path)
+   df_list = df["tourney_name"].drop_duplicates().tolist()
+   dropdown = ttk.Combobox(
+   state="readonly",
+   )
+   dropdown["values"] = df_list #Tournament names
+   dropdown.place(x=50, y=50)
+
+   return df
+
+
+ 
 
 # Create a Tkinter window
 root = tk.Tk()
@@ -22,5 +29,14 @@ root.title("Opening a .csv file using button")
 # Create a button to open the .csv file
 open_button = tk.Button(root, text="Open .csv File", command=open_csv_file)
 open_button.pack()
+
+
+
+
+
+
+
+
+
 # Start the Tkinter main loop to run the GUI application
 root.mainloop() 
